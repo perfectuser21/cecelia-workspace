a1d1b46a6ea509aebd275fed74a3bcf4023a695b
ALex Xu <xx@Perfect2.local>
2026-01-17 21:51:33 +0800

    fix: 恢复飞书扫码登录功能

---
+++ apps/dashboard/frontend/dist/assets/video-C2lj4PgF.js
@@ -0,0 +1,11 @@
+import{c as e}from"./index-BrpX3zYB.js";/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const t=e("MessageCircle",[["path",{d:"m3 21 1.9-5.7a8.5 8.5 0 1 1 3.8 3.8z",key:"v2veuj"}]]);/**
+ * @license lucide-react v0.294.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=e("Video",[["path",{d:"m22 8-6 4 6 4V8Z",key:"50v9me"}],["rect",{width:"14",height:"12",x:"2",y:"6",rx:"2",ry:"2",key:"1rqjg6"}]]);export{t as M,a as V};
