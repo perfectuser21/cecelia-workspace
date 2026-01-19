a9e930e1fed670325b625e9351e19fa77e5b2b86
ALex Xu <xx@Perfect2.local>
2026-01-19 17:00:03 +0800

    auto-backup: 2026-01-19 17:00

---
+++ apps/dashboard/frontend/dist-dev/assets/client-DSYFshK-.js
@@ -0,0 +1,1 @@
+import{a}from"./index-B9ygI19o.js";const e="/api",o="dev-api-key-2025",i=a.create({baseURL:e,headers:{"Content-Type":"application/json",Authorization:`Bearer ${o}`}});export{i as a};
