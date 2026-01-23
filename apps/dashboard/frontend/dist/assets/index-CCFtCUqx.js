636fa71130ab76e72e91a3942c1e56ba9be47129
ALex Xu <xx@Perfect2.local>
2026-01-23 20:00:03 +0800

    auto-backup: 2026-01-23 20:00

---
+++ apps/dashboard/frontend/dist/assets/index-CCFtCUqx.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Tasks-CGwL1otq.js","assets/index-DNhmzR3H.js","assets/index-3UAe3Ji9.css","assets/tiny-invariant-BaFNuDhB.js","assets/index-B9ygI19o.js"])))=>i.map(i=>d[i]);
+import{ah as s}from"./index-DNhmzR3H.js";const t={id:"tasks",name:"Notion Tasks",version:"1.0.0",source:"core",instances:["core"],routes:[{path:"/tasks",component:"Tasks"}],components:{Tasks:()=>s(()=>import("./Tasks-CGwL1otq.js"),__vite__mapDeps([0,1,2,3,4]))}};export{t as default};
